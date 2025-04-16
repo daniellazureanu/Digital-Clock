bfe4ebe40a052bda1a9eaa04b21e785790ab6a88
daniellazureanu <wqj831344@gmail.com>
2025-04-16 18:07:07 +0300

    linux performance update

--- src/clock.cpp
+++ src/clock.cpp
@@ -2,8 +2,10 @@
 #include <chrono>
 #include <thread>
 #include <vector>
-#ifdef _WIN32_
-	#include <conio.h>
+#include <sstream>
+#include <ctime>
+#ifdef _WIN32
+#include <conio.h>
 #endif
 
 #define CLOCK_STARTING_COLUMN 30 //Where the clock is placed (COLUMN, ROW)
@@ -14,16 +16,16 @@
 #define ASCII_DOTS_COLUMNS 4    //Same as above but for the ASCII ART that represents the dots
 #define DOTS_INDEX 10           //The index of the `dots` element in the array
 
-void moveCursor();
 void hideCursor();
 void showCursor();
 void resetText();
-void printTimeUnit(int timeUnit, int& x, int& y);
-void printFirstDots(int& x, int& y);
-void printSecondDots(int& x, int& y);
-void printCurrentTime(int hour, int minute, int second, int x, int y);
+void printTimeUnit(std::ostringstream& out, int timeUnit, int& x, int& y);
+void printFirstDots(std::ostringstream& out, int& x, int& y);
+void printSecondDots(std::ostringstream& out, int& x, int& y);
+void printCurrentTime(std::ostringstream& out, int hour, int minute, int second);
 void clearScreen();
 void changeColors(const std::string& colorAttribute);
+void moveCursor(std::ostringstream& out, int x, int y);
 
 const std::vector<std::vector<std::string>> ascii_digits = {
     {
@@ -118,25 +120,21 @@ const std::vector<std::vector<std::string>> ascii_digits = {
 };
 
 int main() {
-    int hour = 0;
-    int minute = 0;
-    int second = 0;
+    int hour = 0, minute = 0, second = 0;
 
-    int x = 0;
-    int y = 0;
-#ifdef _WIN32
+    #ifdef _WIN32
     system("chcp 65001");
-#endif
+    #endif
 
-#ifdef __linux
+    #ifdef __linux__
     system("stty -echo");
-#endif
+    #endif
+
     clearScreen();
     hideCursor();
     changeColors(COLOR_ATTRIBUTE);
 
     while (true) {
-        // Get the current system time
         auto now = std::chrono::system_clock::now();
         std::time_t now_time = std::chrono::system_clock::to_time_t(now);
         std::tm* local_time = std::localtime(&now_time);
@@ -145,20 +143,19 @@ int main() {
         minute = local_time->tm_min;
         second = local_time->tm_sec;
 
-        // Print the current time
-        printCurrentTime(hour, minute, second, x, y);
+        std::ostringstream out;
+        printCurrentTime(out, hour, minute, second);
+        std::cout << out.str() << std::flush;
 
-        // Sleep until the next second
         now += std::chrono::seconds(1);
         std::this_thread::sleep_until(now);
-	#ifdef _WIN32_
-		if (_kbhit()){
-			char ch = _getch();
-			if(ch == 'q' || ch == 'Q'){
-				break;
-			}
-		}
-	#endif
+
+        #ifdef _WIN32
+        if (_kbhit()) {
+            char ch = _getch();
+            if (ch == 'q' || ch == 'Q') break;
+        }
+        #endif
     }
 
     showCursor();
@@ -166,25 +163,36 @@ int main() {
     return 0;
 }
 
-void moveCursor(int x, int y) {
-    std::cout << "\033[" << y << ";" << x << "H";
+void moveCursor(std::ostringstream& out, int x, int y) {
+    out << "\033[" << y << ";" << x << "H";
 }
 
-void hideCursor(){
+void hideCursor() {
     std::cout << "\033[?25l";
 }
 
-void showCursor(){
-	std::cout<<"\033[?25h";
+void showCursor() {
+    std::cout << "\033[?25h";
 }
 
-void resetText(){
-	std::cout<<"\033[0m";
+void resetText() {
+    std::cout << "\033[0m";
 }
 
-void printTimeUnit(int timeUnit, int& x, int& y){
+void clearScreen() {
+    std::cout << "\033[2J\033[H";
+}
+
+void changeColors(const std::string& colorAttribute) {
+    #ifdef _WIN32
+    system(("color " + colorAttribute).c_str());
+    #elif __linux__
+    std::cout << "\033[32m";
+    #endif
+}
+
+void printTimeUnit(std::ostringstream& out, int timeUnit, int& x, int& y) {
     std::vector<int> digits;
-
     if (timeUnit == 0) {
         digits.push_back(0);
     } else {
@@ -192,84 +200,62 @@ void printTimeUnit(int timeUnit, int& x, int& y){
             digits.insert(digits.begin(), timeUnit % 10);
             timeUnit /= 10;
         }
-    }    
-
-    if(digits.size()==1){
-        for (const auto& line : ascii_digits[0]) {
-            std::cout << line;
-            moveCursor(x,++y);
-        }
-        x+=ASCII_DIGITS_COLUMNS;
-        y=CLOCK_STARTING_ROW;
-        moveCursor(x,y);       
     }
 
-    for(int digit : digits){
+    if (digits.size() == 1) {
+        for (const auto& line : ascii_digits[0]) {
+            moveCursor(out, x, y++);
+            out << line;
+        }
+        x += ASCII_DIGITS_COLUMNS;
+        y = CLOCK_STARTING_ROW;
+    }
+
+    for (int digit : digits) {
         for (const auto& line : ascii_digits[digit]) {
-            std::cout << line;
-            moveCursor(x,++y);
+            moveCursor(out, x, y++);
+            out << line;
         }
-        if(digits.size()>1){
-            x+=ASCII_DIGITS_COLUMNS;
-            y=CLOCK_STARTING_ROW;
-            moveCursor(x,y);
+        if (digits.size() > 1) {
+            x += ASCII_DIGITS_COLUMNS;
+            y = CLOCK_STARTING_ROW;
         }
-    }     
+    }
 }
 
-void printFirstDots(int& x, int& y){
-    	x=CLOCK_STARTING_COLUMN + ASCII_DIGITS_COLUMNS*2+1;
-    	y=CLOCK_STARTING_ROW;
-    	moveCursor(x,y);
-    	for (const auto& line : ascii_digits[DOTS_INDEX]) {
-        	std::cout << line;
-        	moveCursor(x,++y);
-    	}
-    	x=x+ASCII_DOTS_COLUMNS+1;
-    	y=CLOCK_STARTING_ROW;
-    	moveCursor(x,y);
+void printFirstDots(std::ostringstream& out, int& x, int& y) {
+    x = CLOCK_STARTING_COLUMN + ASCII_DIGITS_COLUMNS * 2 + 1;
+    y = CLOCK_STARTING_ROW;
+    for (const auto& line : ascii_digits[DOTS_INDEX]) {
+        moveCursor(out, x, y++);
+        out << line;
+    }
+    x += ASCII_DOTS_COLUMNS + 1;
+    y = CLOCK_STARTING_ROW;
 }
 
-void printSecondDots(int& x, int& y){
-    	x=CLOCK_STARTING_COLUMN+ASCII_DIGITS_COLUMNS*2+1+ASCII_DOTS_COLUMNS+1+ASCII_DIGITS_COLUMNS*2+1;
-    	y=CLOCK_STARTING_ROW;
-    	moveCursor(x,y);
-    	for (const auto& line : ascii_digits[DOTS_INDEX]) {
-        	std::cout << line;
-       	 	moveCursor(x,++y);
-    	}
-    	x=x+ASCII_DOTS_COLUMNS+1;
-    	y=CLOCK_STARTING_ROW;
-    	moveCursor(x,y);
+void printSecondDots(std::ostringstream& out, int& x, int& y) {
+    x = CLOCK_STARTING_COLUMN + ASCII_DIGITS_COLUMNS * 2 + 1 + ASCII_DOTS_COLUMNS + 1 + ASCII_DIGITS_COLUMNS * 2 + 1;
+    y = CLOCK_STARTING_ROW;
+    for (const auto& line : ascii_digits[DOTS_INDEX]) {
+        moveCursor(out, x, y++);
+        out << line;
+    }
+    x += ASCII_DOTS_COLUMNS + 1;
+    y = CLOCK_STARTING_ROW;
 }
 
-void printCurrentTime(int hour, int minute, int second, int x, int y){
-        x=CLOCK_STARTING_COLUMN;
-        y=CLOCK_STARTING_ROW;
-        moveCursor(x,y);
+void printCurrentTime(std::ostringstream& out, int hour, int minute, int second) {
+    int x = CLOCK_STARTING_COLUMN;
+    int y = CLOCK_STARTING_ROW;
 
-        printTimeUnit(hour, x, y);
-        printFirstDots(x, y);
-        printTimeUnit(minute, x, y);
-        printSecondDots(x, y);
-        printTimeUnit(second, x, y);      
+    printTimeUnit(out, hour, x, y);
+    printFirstDots(out, x, y);
+    printTimeUnit(out, minute, x, y);
+    printSecondDots(out, x, y);
+    printTimeUnit(out, second, x, y);
 }
 
-void clearScreen(){
-#ifdef _WIN32
-    	std::cout << "\033[2J\033[H";
-#elif __linux__
-    	system("clear");
-#endif  
-}
-
-void changeColors(const std::string& colorAttribute) {
-#ifdef _WIN32
-	system(("color " + colorAttribute).c_str());
-#elif __linux__
-	std::cout<<"\033[32m";
-#endif
-}
 
 /*METADA FOR THIS FONT 
     CLOCK_STARTING_COLUMN -> 5
